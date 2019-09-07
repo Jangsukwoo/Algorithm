ead233fca9d812cef347d3999788a59410e16f2b
Jangsukwoo <wwwkdtjrdn@naver.com>
2019-09-07 11:40:35 +0900

    nP2

--- Algo/src/순열조합/nPr2.java
+++ Algo/src/순열조합/nPr2.java
@@ -1,5 +1,44 @@
 package 순열조합;
 
-public class nPr2 {
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class nPr2{
+	public static int N;	
+	public static int M;
+	public static int[] data;
+	public static boolean[] visit;
+	public static int[] result;
+	public static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		data = new int[N];
+		result = new int[N];
+		visit = new boolean[N];
+		for(int i=0;i<N;i++) data[i]=sc.nextInt();
+		Arrays.sort(data);
+		dfs(0);
+		System.out.println(sb.toString());
+	}
+
+	private static void dfs(int cur) {
+		if(cur==M) {
+			for(int i=0;i<M;i++) {
+				sb.append(String.valueOf(result[i])+" ");
+			}
+			sb.append("\n");	
+			return;
+		}
+		for(int i=0;i<N;i++) {
+			if(!visit[i]){
+				visit[i] = true;
+				result[cur] = data[i];
+				dfs(cur+1);
+				visit[i] = false;
+			}
+		}
+	}
 }
