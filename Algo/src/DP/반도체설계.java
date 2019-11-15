d71a6210ca7c0feb38f549dacfaf57e80b2de5e6
Jangsukwoo <wwwkdtjrdn@naver.com>
2019-11-16 00:39:49 +0900

    DP

--- Algo/src/DP/반도체설계.java
+++ Algo/src/DP/반도체설계.java
@@ -7,14 +7,22 @@ import java.util.Scanner;
 
 
 public class 반도체설계 {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
             int N = sc.nextInt();
             int[] data = new int[N];
-            int[] memo = new int[N];
+            int[] l = new int[N];
             for(int i=0;i<N;i++) data[i] = sc.nextInt(); 
-            int max=0;
-        
+            int max =0;
+            for(int i=0;i<N;i++) {
+                l[i]=1;
+                for(int j=0;j<=i;j++) {
+                    if(data[j]<data[i] &&l[i]<l[j]+1) {
+                        l[i]=l[j]+1;
+                    }
+                    if(max<l[i])max=l[i];
+                }
+            }
             System.out.println(max);
         
     }
