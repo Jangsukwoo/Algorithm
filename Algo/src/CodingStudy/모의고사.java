e343c9afe8d09167d750da780011aff7b859a25e
Jangsukwoo <wwwkdtjrdn@naver.com>
2019-12-06 12:24:43 +0900

    CodingStudy

---
+++ Algo/src/CodingStudy/모의고사.java
@@ -0,0 +1,57 @@
+package CodingStudy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+ * 20191206 스터디
+ * 규칙을 가진 수열 배열을 하나 만들고
+ * 답이랑 비교하기
+ */
+public class 모의고사 {
+	static int[] series1 = {1,2,3,4,5};
+	static int[] series2 = {2,1,2,3,2,4,2,5};
+	static int[] series3 = {3,3,1,1,2,2,4,4,5,5};
+	static int[] A;
+	static int[] B;
+	static int[] C;
+	static int Amax,Bmax,Cmax;
+	static int Max;
+	static ArrayList<Integer> list = new ArrayList<Integer>();
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(solution(new int[] {1,2,3,4,5})));
+		
+	}
+	public static int[] solution(int[] answers) {
+		int[] answer = {};
+		int size = answers.length;
+		makeSeries(size);
+		for(int i=0;i<size;i++) {
+			if(answers[i]==A[i]) Amax++;
+			if(answers[i]==B[i]) Bmax++;
+			if(answers[i]==C[i]) Cmax++;
+		}
+		if(Max<=Amax) Max = Amax;
+		if(Max<=Bmax) Max = Bmax;
+		if(Max<=Cmax) Max = Cmax;
+		if(Max==Amax) list.add(1);
+		if(Max==Bmax) list.add(2);
+		if(Max==Cmax) list.add(3);
+		answer = new int[list.size()];
+		for(int i=0;i<answer.length;i++) {
+			answer[i] = list.get(i);
+		}
+		return answer;
+	}
+	private static void makeSeries(int size){
+		A = new int[size];
+		B = new int[size];
+		C = new int[size];
+		for(int i=0;i<size;i++){
+			A[i] = series1[i%series1.length];
+			B[i] = series2[i%series2.length];
+			C[i] = series3[i%series3.length];
+		}
+	}
+
+}
---
+++ Algo/src/CodingStudy/소수찾기.java
@@ -0,0 +1,69 @@
+package CodingStudy;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+/*
+ * String인  numbers 문자의 최대 사이즈 7
+ * 17,71 처럼 순서가 있으니 Permutation nPr 
+ * 소수인경우 Set에 넣어서 중복검사
+ */
+public class 소수찾기 {
+	static int prime;
+	static int n,r;
+	static boolean[] visit;
+	static char[] number;
+	static char[] data;
+	static HashSet<Integer> primeSet = new LinkedHashSet<Integer>(); //확인된 소수값
+	public static void main(String[] args) {
+		solution("0002");
+		System.out.println(prime);
+	}
+
+	public static int solution(String numbers) {
+		data = numbers.toCharArray();
+		n = numbers.length();
+		for(int i=1;i<=n;i++){//nPr, nP1 부터 nPn까지 전부 조사
+			visit = new boolean[n];
+			number = new char[i];
+			r = i; 
+			permutation(0);
+		}
+		return prime;
+	}
+	private static void permutation(int cnt) {
+		if(cnt==r){
+			String stringNumber="";
+			for(int i=0;i<r;i++){
+				stringNumber+=number[i];
+			}
+			int num = Integer.parseInt(stringNumber);
+			if(num==2 && !primeSet.contains(num)){//2인경우만 봐준다.
+				primeSet.add(2);
+				prime++;
+				return;
+			}
+			if(num==1 || num==0 || num%2==0 || primeSet.contains(num)) return; //짝수나 1,0,이미 카운팅한 소수는 거른다 
+			isPrime(num);//위 조건에 안걸렸으면 소수인지 검사
+			return;
+		}
+		for(int i=0;i<n;i++){
+			if(visit[i]==false){
+				visit[i] = true;
+				number[cnt] = data[i];
+				permutation(cnt+1);
+				visit[i] = false;
+			}
+		}
+	}
+	private static void isPrime(int num) {
+		for(int i=3;i<num;i+=2){	
+			if((num%i)==0) {
+				return; //어떤 수와 나눠지면 소수가 아니니까 끝낸다.
+			}
+		}
+		//위 포문에서 안걸렸으면 카운팅
+		primeSet.add(num); //소수 집합에 추가
+		prime++; //카운팅
+	}
+}
