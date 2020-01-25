b61d0b2c9d63af25c227ffef3bbe41dc025563f0
Jangsukwoo <wwwkdtjrdn@naver.com>
2020-01-25 18:14:37 +0900

    Review

---
+++ Algo/src/CodingStudySamsungProblem/구슬탈출2.java
@@ -0,0 +1,42 @@
+package CodingStudySamsungProblem;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+/*
+ * 빨간 구슬을 구멍을 통해 빼낸다.
+ * 단, 파란구슬은 구멍에 빠지면 안된다.
+ * 구슬을 움직이는 방법은
+ * 판을 상,하,좌,우로 기울인다
+ * 
+ * 성공 기준은
+ * 빨간 구슬만 구멍에 빠지는 것이고
+ * 파란 구슬이 빠지거나 
+ * 빨강,파랑 구슬이 동시 에 빠지면 실패이다.
+ * 구슬이 더이상 움직이지 않을 때 게임은 멈추고
+ * 보드의 상태가 주어졌을 때 
+ * 최소 몇 번 만에 빨간 구슬을 빼낼 수 있는지 구하라
+ * 단, 10번의 시도로 구슬을 꺼낼 수 없으면
+ * -1을 출력하고 프로그램을 끝낸다.
+ */
+public class 구슬탈출2{
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static char[][] board;
+	static int N,M;//세로,가로
+	public static void main(String[] args) throws IOException {
+		setData();
+	}
+	private static void setData() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); //세로 
+		M = Integer.parseInt(st.nextToken()); //가로
+		board = new char[N][M];
+		for(int row=0;row<N;row++) board[row] = br.readLine().toCharArray();
+		
+	}
+}
--- Algo/src/SDS복습/자료구조_구간합구하기_세그먼트트리.java
+++ Algo/src/SDS복습/자료구조_구간합구하기_세그먼트트리.java
@@ -23,6 +23,8 @@ public class 자료구조_구간합구하기_세그먼트트리 {
 	public static void main(String[] args) throws IOException {
 		setData();
 		getAnswer();
+		bw.flush();
+		bw.close();
 	}
 	private static void getAnswer() throws IOException {
 		for(int i=0;i<(M+K);i++){
@@ -33,24 +35,31 @@ public class 자료구조_구간합구하기_세그먼트트리 {
 			switch (a) {
 			case 1://b를 c로 바꾼다.		
 				long diff =  c - number[b-1];
+				number[b-1] = c; //<- number값도 업데이트 해줘야한다.
 				updateSegmentTree(1,0,N-1,diff,b-1);
 				break;
 			case 2://b부터 c사이의 합을 출력 
 				long sum = getSumFromSegmentTree(1,0,N-1,b-1,c-1);
+				bw.write(sum+"\n");
 				break;
 			}
 		}
 	}
 	private static long getSumFromSegmentTree(int node, int start, int end, int left, int right) {
-		if(end<=left && right<=start) return 0;//아예 범위가 말이 안되는경우 
-		return 0;
+		if(end<left || right<start) return 0;//아예 범위가 말이 안되는경우 
+		if(left<=start && end<=right) return segmentTree[node];//영역이 걸쳐있는 경우. 즉,4~9의 구간의 합이면, 5~6의 합은 당연히 속해있다.
+		int mid = (start+end)/2;
+		return getSumFromSegmentTree(node*2, start, mid, left, right)+getSumFromSegmentTree((node*2+1), mid+1, end, left, right);
 	}
 	private static void updateSegmentTree(int node, int start, int end, long diff, int idx) {
 		if(!(start<=idx && idx<=end)) return;//영역안에 속해있지 않으면 끝내버린다.
 		segmentTree[node]+=diff;
-		int mid = (start+end)/2;
-		updateSegmentTree(node*2, start, mid, diff, idx);
-		updateSegmentTree((node*2+1), start, mid, diff, idx);
+		if(start!=end) {
+			int mid = (start+end)/2;	
+			updateSegmentTree(node*2, start, mid, diff, idx);
+			updateSegmentTree((node*2+1), mid+1, end, diff, idx);
+		}
+
 	}
 	private static void setData() throws IOException {
 		st = new StringTokenizer(br.readLine());
@@ -58,7 +67,7 @@ public class 자료구조_구간합구하기_세그먼트트리 {
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		number = new long[N];
-		segmentTree = new long[N*4];
+		segmentTree = new long[N*4+1];
 		for(int i=0;i<N;i++){
 			st = new StringTokenizer(br.readLine());
 			number[i] = Integer.parseInt(st.nextToken());
