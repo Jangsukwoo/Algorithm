ee4bc77fee413985c8d2a7b527881b605e1d3b4a
Jangsukwoo <wwwkdtjrdn@naver.com>
2020-03-09 21:12:36 +0900

    369

---
+++ Algo/src/BAEKJOON/삼육구.java
@@ -0,0 +1,20 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class 삼육구{
+	static int N;
+	static int clap;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		for(int i=1;i<=N;i++){
+			String num = Integer.toString(i);
+			char[] data = num.toCharArray();
+			for(int j=0;j<data.length;j++) {
+				if(data[j]=='3' || data[j]=='6' || data[j]=='9') clap++;
+			}
+		}
+		System.out.println(clap);
+	}
+}
