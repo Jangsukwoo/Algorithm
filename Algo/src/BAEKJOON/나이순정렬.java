95e1591280c3afaaa3cf6a314b34566dece62c87
Jangsukwoo <wwwkdtjrdn@naver.com>
2020-04-14 19:55:00 +0900

    primePath

--- Algo/src/BAEKJOON/나이순정렬.java
+++ Algo/src/BAEKJOON/나이순정렬.java
@@ -7,8 +7,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import java.util.StringTokenizer;
 
 /*
