e306428067386212e2ee835b8f17509d8f989523
Jangsukwoo <wwwkdtjrdn@naver.com>
2020-05-14 16:49:49 +0900

    BinarySearch

--- Algo/src/구현/피자굽기.java
+++ Algo/src/구현/피자굽기.java
@@ -27,20 +27,37 @@ public class 피자굽기 {
 	static long[] dough;
 	static long[] oven;
 	static long[] dp;
-	static int doughIdx;
+	static boolean[] visit;
 	static int answer;
+	static int mid;
+	static boolean flag = true;
 	public static void main(String[] args) throws IOException {
 		setData();
 		memoization();
-		simulation();
-		if(doughIdx==N) System.out.println(answer+1);
+		binarySearchAll();
+		answer=mid;
+		if(flag) System.out.println(answer+1);
 		else System.out.println("0");
 	}
-	private static void simulation() {
+	private static void binarySearchAll() {
 		for(int i=0;i<N;i++) {
-			int idx = Arrays.binarySearch(dp,dough[i]);
-			System.out.println(idx);
+			binarySearch(dough[i]);
+			if(flag==false) return;
 		}
+	}
+	private static void binarySearch(long pizza){
+		flag = false;
+		int start = 0;
+		int end = mid-1;
+		int m=0;
+		while(start<=end){//이분탐색+UpperBound
+			m = (start+end)/2;
+			if(dp[m]>=pizza){
+				flag = true;
+				start = m+1;
+			}else end = m-1;
+		}
+		mid = end;
 	}
 	private static void memoization(){
 		//적은 지름 밑 큰 지름은 의미가 없으니 전부 커팅하는 작업
@@ -61,5 +78,6 @@ public class 피자굽기 {
 		for(int i=0;i<D;i++) oven[i] = Long.parseLong(st.nextToken());
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<N;i++) dough[i] = Long.parseLong(st.nextToken());
+		mid = D;
 	}
 }
