d0e336cc329a3a272305d23276ad2be7248cdcaf
Jangsukwoo <wwwkdtjrdn@naver.com>
2020-06-11 21:51:10 +0900

    powerSet

--- Algo/src/CodingAcademy/부분집합구하기.java
+++ Algo/src/CodingAcademy/부분집합구하기.java
@@ -1,28 +1,46 @@
 package CodingAcademy;
 
+import java.util.Arrays;
+
 public class 부분집합구하기 {
-	static int[] set = new int[] {1,2,3};
+	static int[] set = new int[] {-1,1,1,1,1,1};
 	static int answer;
 	static boolean[] include;
+	static int[] pick;
+	static int cnt=0;
 	public static void main(String[] args){
 		include = new boolean[3];
-		System.out.println(numSubsets(3,2));
+		pick = new int[4];
+		System.out.println(numSubsets(4,4));
+		//nCr(0,0);
+		System.out.println(cnt);
 	}
+	
+	private static void nCr(int idx, int depth){
+		if(depth==4){
+			System.out.println(Arrays.toString(pick));
+			return ;
+		}
+		for(int i=idx;i<7;i++) {
+			pick[depth] = set[i];
+			nCr(i+1,depth+1);
+		}
+		return ;
+	}
+
+	
+
+	
 	private static int numSubsets(int n, int k) {
-		if(k==-1) {
-			for(int i=0;i<n;i++) {
-				if(include[i]) {
-					System.out.print(set[i]+" ");
-				}
-			}
-			System.out.println();
+		cnt++;
+		if(k==0){
 			return 1;
 		}
-		include[k-1] = true;
-		numSubsets(n, k-1);
-		include[k-1] = false;
-		numSubsets(n, k-1);
-		return 0;
 		
+		int sum=0;
+		for(int i=n;i>=1;i--){
+			sum += numSubsets(i-1, k-1);
+		}
+		return sum;
 	}
 }
