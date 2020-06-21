1cb11a6a910f8dd5b3be90323fd07c88042e4940
Jangsukwoo <wwwkdtjrdn@naver.com>
2020-06-21 17:10:21 +0900

    AOV_Network

---
+++ Algo/src/CodingAcademy/AOV네트워크_C언어.java
@@ -0,0 +1,46 @@
+package CodingAcademy;
+
+public class AOV네트워크_C언어 {
+	public static void main(String[] args) {
+		/*
+01: typedef struct node *nPointer;
+02: typedef struct {
+03:         int vertex;
+04:         nPointer link;
+05:         } node;
+06: typedef struct {
+07:         int count;
+08:         nPointer link;
+09:         } heads;
+10: 
+11: bool isWorkable(heads graph[], int n) {
+12:    int i, j, k, t=-1;
+13:    nPointer ptr;
+14:    for (i=0; i<n; i++) {
+15:       if (graph[i].count == 0) {
+16:           graph[i].count = t;
+17:           t = i;
+18:       }
+19:    }
+20:    for (i=0; i<n; i++) { 
+21:       if (t == -1)
+22:          return false;
+23:       else {
+24:          j = t;
+25:          t = ㉠
+26:          for (ptr = graph[j].link; ptr; 
+                 ptr = ptr->link) {
+27:             k = ㉡
+28:             graph[k].count--;
+29:             if (graph[k].count == 0) {
+30:                graph[k].count = t;
+31:                t = k;
+32:             }
+33:          }
+34:       }
+35:    }
+36:    return true;
+37: }
+		 */
+	}
+}
