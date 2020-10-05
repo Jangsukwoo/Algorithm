656e53fafe5813c043a825373cb389e0d115bf5e
Jangsukwoo <wwwkdtjrdn@naver.com>
2020-10-05 21:37:14 +0900

    jumpKingJelly

--- Algo/src/SamsungDS/신기한소수.java
+++ Algo/src/SamsungDS/신기한소수.java
@@ -10,44 +10,31 @@ public class 신기한소수 {
 	static int N;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static String start_string = "2";
-	static String end_string = "9";
-	static int[] startArray = new int[] {2,3,4,5,7};
-	static boolean impossible = false;
+	static int[] startArray = new int[] {2,3,5,7};
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		N = Integer.parseInt(br.readLine());
-		for(int s=0;s<startArray.length;s++) {
-			start_string = Integer.toString(startArray[s]);
-			end_string = start_string+"9";
-			for(int i=1;i<=(N-1);i++) {
-				start_string+="0";
-			}
-			for(int i=2;i<=(N-1);i++) {
-				end_string+="9";
-			}
-			int start = Integer.parseInt(start_string);
-			int end = Integer.parseInt(end_string);
-			for(int num = start;num<=end;num++) {
-				if(num%2==0) continue; //짝수로 나눠지는 거르고
-				impossible = false;
-				for(int pos = 1; pos<=N;pos++){
-					String num_string = Integer.toString(num);
-					String checkNum_string = num_string.substring(0,pos);
-					int checkNum = Integer.parseInt(checkNum_string);
-					if(checkNum!=2 && checkNum%2==0) {
-						impossible = true;
-						break;
-					}
-					for(int i=3;i<=Math.sqrt(checkNum);i++) {
-						if(checkNum%i==0){
-							impossible = true;//하나라도 나눠떨어지는게 있으면 true
-							break;
-						}
-					}
-					if(impossible) break;
-				}
-				if(impossible==false) System.out.println(num);
+		for(int s=0;s<startArray.length;s++) {//시작 수 
+			dfs(startArray[s],0);
+		}
+	}
+	private static void dfs(int checkNum, int depth) {
+		if(depth==(N-1)) {
+			System.out.println(checkNum);
+			return;
+		}
+		checkNum*=10;
+		for(int i=1;i<=10;i+=2){
+			//System.out.println((checkNum+i));
+			if(isPrime(checkNum+i)){//소수인가?
+				//소수라면
+				dfs(checkNum+i,depth+1);
 			}
 		}
+	}
+	private static boolean isPrime(int temp) {
+		for(int j=3;j<=Math.sqrt(temp);j++){
+			if(temp%j==0) return false;
+		}
+		return true;
 	}
 }
