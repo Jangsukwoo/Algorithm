5bb8d811f77189b71efc2bd6ca5fadfc4bdc33bf
Jangsukwoo <wwwkdtjrdn@naver.com>
2020-10-11 22:05:31 +0900

    coding

--- Algo/src/순열조합/nCr.java
+++ Algo/src/순열조합/nCr.java
@@ -1,4 +1,5 @@
 package 순열조합;
+import java.util.Arrays;
 /*
  * r이 3개나 4개정도로 작을 땐
  * for(int i=0...)
@@ -8,28 +9,30 @@ package 순열조합;
 import java.util.Scanner;
 public class nCr{
 	public static int[] data;
-	public static boolean[] visit;
+	public static int[] pick;
 	public static int N;
 	public static int M;
-	public static void nCrDFS(int idx, int cnt) {
-		if(cnt==M) {
-			for(int i=0;i<N;i++) if(visit[i]) System.out.print(data[i]+" ");
-			System.out.println();
+	static int nCrCount;
+	public static void nCrDFS(int idx, int depth) {
+		if(depth==M) {
+			nCrCount++;
+			System.out.println(Arrays.toString(pick));
 			return;
 		}
 		for(int i=idx; i<N; i++) {
-			visit[i]=true;
-			nCrDFS(i+1, cnt+1);
-			visit[i]=false;
+			pick[depth] = data[i];
+			nCrDFS(i+1, depth+1);
 		}
+		
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt(); //데이터 사이즈
 		M = sc.nextInt(); //보고싶은 케이스의 사이즈
 		data = new int[N];
-		visit = new boolean[N];
+		pick = new int[M];
 		for(int i=0;i<N;i++) data[i] = i+1;
 		nCrDFS(0, 0);
+		System.out.println("N:"+N+" "+"M:"+M+" "+"nCr의 결과 :" + nCrCount);
 	}
 }
